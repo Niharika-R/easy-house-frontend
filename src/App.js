2508f3fbba5b34284aaca43a59c50c6ee3044ff8
niharka2502m@gmail.com <niharika@altoira.com>
2022-10-06 16:36:13 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pages from "./pages";
 import { UserAuthentication } from "./context/UserAuthentications";
+import Appbar from './components/Appbar'
 
 function App() {
   return (
     <UserAuthentication>
     <div className="App">
       <Router>
+      <Appbar/>
         <Routes>
           {Object.values(Pages).map((page) => (
             <Route key={page.path} path={page.path} element={page.element}/>
